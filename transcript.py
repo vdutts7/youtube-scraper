e7214b0d5859cd60ca6fb793e51afe20bef5c26a
vd7 <me@vd7.io>
2024-05-03 00:08:44 -0700

    yt transcript --> JSON with output file

--- transcript.py
+++ transcript.py
@@ -1,18 +1,17 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 url = 'https://www.youtube.com/watch?v=vEd-LqBCONg'
-print(url)
-
 video_id = url.replace('https://www.youtube.com/watch?v=', '')
-print(video_id)
 
 transcript = YouTubeTranscriptApi.get_transcript(video_id)
 
-print(transcript)
+output = ''
+for entry in transcript:
+    sentence = entry['text']
+    output += f'{sentence}\n'
 
-output=''
-for x in transcript:
-  sentence = x['text']
-  output += f' {sentence}\n'
+# Write the output to a file
+with open(f'{video_id}_transcript.txt', 'w', encoding='utf-8') as file:
+    file.write(output)
 
-print(output)
+print(f"Transcript has been saved to {video_id}_transcript.txt")
